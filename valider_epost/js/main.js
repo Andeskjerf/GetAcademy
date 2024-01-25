a086d2796e6804941edebb66676da6508aac570b
Andeskjerf <andreas.skjerven@pm.me>
2024-01-25 14:21:27 +0100

    valider epost prosjekt: del 2 & 3

--- valider_epost/js/main.js
+++ valider_epost/js/main.js
@@ -1,9 +1,32 @@
 let valid = false
 
 function validateEmail(elem) {
-  let parts = elem.value.split('@')
-	valid = parts.length > 1
-	view()
+	valid = false
+  let foundA = false
+  let foundPeriod = false
+  // regex would be nicer for this :(
+  for (let i = 0; i < elem.value.length; i++) {
+    if (elem.value[i] == '.' && foundA) {
+      foundPeriod = true
+    } else if (elem.value[i] == '.' && !foundA) {
+      valid = false
+      break
+    }
+    if (elem.value[i] == ' ') {
+      valid = false
+      break
+    }
+    if (elem.value[i] == '@') {
+      foundA = true
+    }
+  }
+
+		console.log(foundA, foundPeriod)
+  if (foundA && foundPeriod) {
+    valid = true
+  }
+
+  view()
 }
 
 function view() {
