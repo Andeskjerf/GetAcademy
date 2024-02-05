c685ce22427113379ff7184456aa60cc64668d38
Andeskjerf <andreas.skjerven@pm.me>
2024-02-05 11:09:58 +0100

    add carousel functionality

--- Slideshow/js/main.js
+++ Slideshow/js/main.js
@@ -1,15 +1,44 @@
 const totalImages = 3
 let currentSlide = 0
 
+function updateSlides() {
+  let images = document.querySelectorAll('#carousel img')
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('toTheLeft')
+    images[i].classList.remove('toTheRight')
+    if (i < currentSlide) {
+      images[i].classList.add('toTheLeft')
+    } else if (i > currentSlide) {
+      images[i].classList.add('toTheRight')
+    }
+  }
+}
+
+function buttonPressed(text) {
+  switch (text) {
+    case 'Previous':
+      currentSlide--
+      if (currentSlide < 0) {
+        currentSlide = totalImages - 1
+      }
+      break
+    case 'Next':
+      currentSlide++
+      if (currentSlide >= totalImages) {
+        currentSlide = 0
+      }
+      break
+  }
+  updateSlides()
+}
+
 function drawCarousel() {
   let html = `<div id="carousel">`
-
   for (let i = 0; i < totalImages; i++) {
     html += `
 			<img src="assets/${i}.jpg" />
 		`
   }
-
   html += '</div>'
   return html
 }
@@ -37,4 +66,5 @@ function view() {
 		</div>
 		${drawButtons()}
 	`
+  updateSlides()
 }
