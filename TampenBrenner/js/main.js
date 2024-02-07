a32a8a13f1de16d4d26c6ac780012f4be59a7647
Andeskjerf <andreas.skjerven@pm.me>
2024-02-07 10:43:24 +0100

    find distance & change opacity

--- TampenBrenner/js/main.js
+++ TampenBrenner/js/main.js
@@ -1,3 +1,31 @@
+document.body.addEventListener('mousemove', mouseMoved)
+
+let pos
+let onDot = false
+let distancePercentage = 100
+
+function getDistance(x1, y1, x2, y2) {
+  let xDistance = x2 - x1
+  let yDistance = y2 - y1
+  return Math.sqrt(xDistance * xDistance + yDistance * yDistance)
+}
+
+function mouseMoved(e) {
+  let x = e.clientX
+  let y = e.clientY
+
+  let compressedValue = ((getDistance(x, y, pos.x, pos.y) * 100) / window.innerWidth) * 10
+  distancePercentage = 100 - compressedValue
+
+  if (e.target.id == 'dot') {
+    onDot = true
+  } else {
+    onDot = false
+  }
+
+  view()
+}
+
 function getRandomXY() {
   let x = Math.floor(Math.random() * window.innerWidth)
   let y = Math.floor(Math.random() * window.innerHeight)
@@ -5,13 +33,24 @@ function getRandomXY() {
 }
 
 function drawDot() {
-	let pos = getRandomXY()
-  return `<div id="dot" style="top: ${pos.y}px; left: ${pos.x}px;"></div>`
+  if (pos) {
+    return `<div id="dot" style="top: ${pos.y}px; left: ${pos.x}px; opacity: ${distancePercentage}%;"></div>`
+  }
+  pos = getRandomXY()
+  return `<div id="dot" style="top: ${pos.y}px; left: ${pos.x}px; opacity: ${distancePercentage}%;"></div>`
+}
+
+function drawOnDotText() {
+  if (onDot) {
+    return `<h1 id="dotFound">Du fant dotten!</h1>`
+  }
+  return ''
 }
 
 function view() {
   let app = document.getElementById('app')
   app.innerHTML = `
 		${drawDot()}
+		${drawOnDotText()}
 	`
 }
